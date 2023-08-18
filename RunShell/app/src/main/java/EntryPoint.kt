64c4ed60d4e3f5c43a1d994804499be22967cb91
goncalopalaio <goncalo.palaio@gmail.com>
2023-08-19 00:56:22 +0100

    Screenshot POC

--- RunShell/app/src/main/java/EntryPoint.kt
+++ RunShell/app/src/main/java/EntryPoint.kt
@@ -1,5 +1,6 @@
 import runner.InjectKeyEvents
 import runner.SetClipboard
+import runner.TakeScreenshot
 
 private const val ERROR_NO_PROGRAM = 1000
 private const val ERROR_UNKNOWN_PROGRAM = 1001
@@ -18,6 +19,7 @@ fun start(args: Array<String>): Int {
     return when (program) {
         "clipboard" -> SetClipboard.run(arguments)
         "input" -> InjectKeyEvents.run(arguments)
+        "screenshot" -> TakeScreenshot.run(arguments)
         "help" -> runHelp()
         else -> {
             println("Unknown program -> $program")
---
+++ RunShell/app/src/main/java/runner/TakeScreenshot.kt
@@ -0,0 +1,84 @@
+package runner
+
+import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.Point
+import android.graphics.Rect
+import android.os.Environment
+import java.io.File
+import kotlin.system.measureTimeMillis
+
+object TakeScreenshot {
+
+    @Suppress("UNUSED_PARAMETER")
+    fun run(args: List<String>): Int {
+        val startScreenshotTimeMs = System.currentTimeMillis()
+        val bitmap = screenshot()
+        val screenshotTimeMs = System.currentTimeMillis() - startScreenshotTimeMs
+        println("bitmap=$bitmap, width=${bitmap?.width}, height=${bitmap?.height}, screenshotTimeMs=$screenshotTimeMs")
+        if (bitmap == null) return 1
+
+        val file = File(Environment.getExternalStorageDirectory(), "${System.currentTimeMillis()}.jpg")
+
+        val writeTimeMs = measureTimeMillis {
+            writeBitmap(bitmap, Bitmap.CompressFormat.JPEG, 50, file)
+        }
+
+        val recycleTimeMs = measureTimeMillis {
+            bitmap.recycle()
+        }
+
+        println("file=$file, screenshotTimeMs=$screenshotTimeMs, writeTimeMs=$writeTimeMs, recycleTimeMs=$recycleTimeMs")
+        return 0
+    }
+
+    fun createHelp(): String = """""".trimIndent()
+
+    @Throws(Exception::class)
+    fun screenshot(): Bitmap? {
+        val size: Point = getDisplaySize()
+        println("size=$size")
+
+        // https://cs.android.com/android/platform/superproject/main/+/main:frameworks/base/core/java/android/view/SurfaceControl.java;l=859;drc=ebd11defff68784e4c049e8ef7668f4dc007e2ce;bpv=1;bpt=1?q=SurfaceControl
+
+        val clazz = Class.forName("android.view.SurfaceControl")
+        println("class=$clazz")
+
+        val methods = clazz.methods
+        for (m in methods) {
+            println("m=$m")
+        }
+
+        val method = clazz.getMethod(
+            "screenshot",
+            Rect::class.java,
+            Int::class.javaPrimitiveType,
+            Int::class.javaPrimitiveType,
+            Int::class.javaPrimitiveType,
+        )
+        println("method=$method")
+
+        // VERSION.SDK_INT <= 17 android.view.Surface.screenshot(size.x, size.y)
+        // VERSION.SDK_INT < 28  SurfaceControl.screenshot(size.x, size.y)
+
+        val rotation = 0
+        return method.invoke(null, *arrayOf(Rect(), size.x, size.y, rotation)) as? Bitmap
+    }
+
+    private fun getDisplaySize(): Point {
+        val displayMetrics = Resources.getSystem().displayMetrics
+        return Point(displayMetrics.widthPixels, displayMetrics.heightPixels)
+    }
+
+    private fun writeBitmap(
+        bitmap: Bitmap,
+        format: Bitmap.CompressFormat,
+        quality: Int,
+        file: File
+    ) {
+        file.outputStream().use { out ->
+            bitmap.compress(format, quality, out)
+            out.flush()
+        }
+    }
+}
