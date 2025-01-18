6a6231d8bbcdafded9c733c99657e03368a95c31
goncalopalaio <goncalo.palaio@gmail.com>
2025-01-18 23:17:25 +0000

    Adds test script for development

---
+++ RunShell/test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+command=$1
+args=$2
+
+path_to_apk="app/build/intermediates/apk/debug/app-debug.apk"
+# path_to_apk="app/build/intermediates/apk/release/run-as-shell.apk"
+
+test_apk="run-as-shell.apk"
+
+function install {
+  cp $path_to_apk $test_apk
+  adb push $test_apk /data/local/tmp
+}
+
+if [ "$command" = "install" ]; then
+    install
+
+elif [ "$command" = "test-input-1" ];then
+    adb shell CLASSPATH=/data/local/tmp/run-as-shell.apk app_process / Runner input "9-SLEEP200-7-SLEEP200-12"
+else
+	echo "Command not supported | command="$command", args="$args
+fi
